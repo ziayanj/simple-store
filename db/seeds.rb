119cd84afd672d9cefa87432144a37f5b41e69c9
Ziayan Javed <ziayanj@gmail.com>
2022-03-24 22:59:14 +0500

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Cream Cheese',
+               description: 'Delicious cheese for your bagel.',
+               price: 3.34)
+
+Product.create(name: 'Shoe Phone',
+               description: 'All super spies have phones in their shoes.',
+               price: 83.99)
+
+Product.create(name: 'Robot Vacuum',
+               description: 'Cleans your floors while you drink wine.',
+               price: 113.34)
