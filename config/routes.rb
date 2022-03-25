bb088530176343b6eab9e7f520b012efe7dbf9d9
Ziayan Javed <ziayanj@gmail.com>
2022-03-25 14:35:57 +0500

    Fix required routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
+  get 'products', to: 'products#index', as: 'products'
+  get 'products/:id', to: 'products#show', as: 'product'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
