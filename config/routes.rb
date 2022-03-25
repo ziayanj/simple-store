d888397831d937191be01eafd300fdbad84eb543
Ziayan Javed <ziayanj@gmail.com>
2022-03-25 15:57:38 +0500

    Display individual products from the numeric id

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get 'products', to: 'products#index', as: 'products'
-  get 'products/:id', to: 'products#show', as: 'product'
+  get 'products/:id', to: 'products#show', as: 'product', id: /\d+/
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
